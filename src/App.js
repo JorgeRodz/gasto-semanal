aeba545b8076f52a4f9e7a709a3db0c4d8da9ed1
Jorge Alberto Rodriguez Flores <jarf4321@gmail.com>
2021-10-20 14:43:34 -0500

    Finish the functionality of the project

--- src/App.js
+++ src/App.js
@@ -1,20 +1,65 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Pregunta from "./components/Pregunta";
+import Formulario from "./components/Formulario";
+import Listado from "./components/Listado";
+import ControlPresupuesto from "./components/ControlPresupuesto";
 
 function App() {
   /*---------------- states --------------------*/
-  const [presupeusto, guardarPresupuesto] = useState(0);
+  const [presupuesto, guardarPresupuesto] = useState(0);
   const [restante, guardarRestante] = useState(0);
+  const [mostrarPregunta, actualizarPregunta] = useState(true);
+  const [gastos, guardarGastos] = useState([]);
+  const [gasto, guardarGasto] = useState({});
+  const [crearGasto, guardarCrearGasto] = useState(false);
+
+  // useEffect que actualiza el restante
+  useEffect(() => {
+    // agrega el nuevo presupuesto
+    if (crearGasto) {
+      guardarGastos([...gastos, gasto]);
+
+      const presupuestoRestante = restante - gasto.cantidad;
+      guardarRestante(presupuestoRestante);
+
+      guardarCrearGasto(false);
+    }
+  }, [gasto, crearGasto, gastos, restante]);
 
   return (
     <div className="container">
       <header>
         <h1>Gasto semanal</h1>
         <div className="contenido-principal contenido">
-          <Pregunta
-            guardarPresupuesto={guardarPresupuesto}
-            guardarRestante={guardarRestante}
-          />
+          {/* prettier-ignore */}
+          {mostrarPregunta ? (
+            <Pregunta
+              guardarPresupuesto={guardarPresupuesto}
+              guardarRestante={guardarRestante}
+              actualizarPregunta={actualizarPregunta}
+            />
+          ) : (
+            <div className="row">
+              <div className="one-half column">
+                {/* prettier-ignore */}
+                <Formulario
+                  guardarGasto={guardarGasto}
+                  guardarCrearGasto={guardarCrearGasto}
+                />
+              </div>
+              <div className="one-half column">
+                {/* prettier-ignore */}
+                <Listado
+                  gastos={gastos}
+                />
+
+                <ControlPresupuesto
+                  presupuesto={presupuesto}
+                  restante={restante}
+                />
+              </div>
+            </div>
+          )}
         </div>
       </header>
     </div>
